8de8572317590ff9aeca21dc4f6a0bc3b6ee0170
Cristopher <cristophernamchee12@gmail.com>
2026-03-08 21:32:53 +0700

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { type Component, createSignal } from 'solid-js';
 
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import { TextField, TextFieldInput } from '@/components/ui/TextField';
+import { GithubIcon } from './components/icons/Github';
+import { HeartIcon } from './components/icons/Heart';
+import { NamcheeIcon } from './components/icons/Namchee';
+import { SolidJsIcon } from './components/icons/Solid';
 import { ScheduleHint } from './components/ScheduleHint';
 import type { ScheduleFormat } from './types';
 
@@ -16,7 +20,7 @@ const App: Component = () => {
   const [value, setValue] = createSignal('');
 
   return (
-    <div class="max-w-3xl font-sans w-full mx-auto mt-24 flex flex-col items-center border border-border rounded-md p-4">
+    <div class="flex-1 max-w-3xl font-sans w-full mx-auto pt-24 flex flex-col items-center rounded-md p-4">
       <Tabs value={format()} onChange={setFormat} class="flex flex-col justify-center w-full">
         <TabsList class="relative rounded-full mx-auto">
           <TabsTrigger value="cron" class="rounded-full cursor-pointer">
@@ -41,6 +45,25 @@ const App: Component = () => {
 
         <ScheduleHint format={format()} index={0} />
       </Tabs>
+
+      <footer class="flex justify-between pt-4 border-t border-border w-full mt-auto text-gray-400 text-sm">
+        <div class="flex items-center">
+          Made in 2026 with <HeartIcon class="mx-1 w-4 h-4" /> and{' '}
+          <a href="https://www.solidjs.com/" target="_blank" rel="noopener noreferrer">
+            <SolidJsIcon class="mx-1 w-4 h-4" />
+          </a>
+          by{' '}
+          <a href="https://www.namchee.dev" target="_blank" rel="noopener noreferrer">
+            <NamcheeIcon class="mt-[2px] ml-[1px] w-6 h-6 text-gray-500" />
+          </a>
+        </div>
+
+        <div class="flex items-center">
+          <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
+            <GithubIcon class="w-4 h-4" />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
