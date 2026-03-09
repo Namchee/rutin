0f84faee9b1478a054097fc19876a4202b0c4c65
Cristopher <cristophernamchee12@gmail.com>
2026-03-09 21:17:03 +0700

    chore: simplify footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Tabs, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import { TextField, TextFieldInput } from '@/components/ui/TextField';
 
 import type { ScheduleFormat } from '@/types';
+import { BranchIcon } from './components/icons/Branch';
 
 const Placeholders = {
   cron: '* * * * *',
@@ -22,7 +23,7 @@ const App: Component = () => {
   const [value, setValue] = createSignal('');
 
   return (
-    <div class="flex-1 max-w-3xl font-sans w-full mx-auto pt-24 flex flex-col items-center rounded-md p-4">
+    <div class="flex-1 max-w-3xl font-sans w-full mx-auto pt-24 pb-8 flex flex-col items-center rounded-md p-4">
       <Tabs value={format()} onChange={setFormat} class="flex flex-col justify-center w-full">
         <TabsList class="relative rounded-full mx-auto">
           <TabsTrigger value="cron" class="rounded-full cursor-pointer">
@@ -48,20 +49,29 @@ const App: Component = () => {
         <ScheduleHint format={format()} index={0} />
       </Tabs>
 
-      <footer class="flex justify-between pt-4 border-t border-border w-full mt-auto text-gray-400 text-sm">
+      <footer class="flex flex-col gap-2 items-center w-full mt-auto text-gray-400 text-sm">
         <div class="flex items-center">
           Made in 2026 with <HeartIcon class="mx-1 w-4 h-4" /> and{' '}
           <a href="https://www.solidjs.com/" target="_blank" rel="noopener noreferrer">
-            <SolidJsIcon class="mx-1 w-4 h-4" />
+            <SolidJsIcon class="mx-1 w-4 h-4 opacity-80 transition-opacity hover:opacity-100" />
           </a>
           by{' '}
           <a href="https://www.namchee.dev" target="_blank" rel="noopener noreferrer">
-            <NamcheeIcon class="mt-[2px] ml-[1px] w-6 h-6 text-gray-500" />
+            <NamcheeIcon class="mt-[2px] ml-[1px] w-6 h-6 text-gray-400 transition-colors hover:text-gray-500" />
           </a>
         </div>
 
-        <div class="flex items-center">
-          <a href="https://github.com/Namchee/rutin" target="_blank" rel="noopener noreferrer">
+        <div class="flex items-center gap-2">
+          <span class="font-mono flex items-center gap-1 text-gray-400 hover:text-gray-500 transition-colors">
+            <BranchIcon class="w-4 h-4" />
+            ffac537
+          </span>
+          •
+          <a
+            href="https://github.com/Namchee/rutin"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-gray-400 hover:text-gray-500 transition-colors">
             <GithubIcon class="w-4 h-4" />
           </a>
         </div>
