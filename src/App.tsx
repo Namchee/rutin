ff1bcbb042330dda438fdaa6d8c9d7e63f3b35be
Cristopher <cristophernamchee12@gmail.com>
2026-03-09 21:23:08 +0700

    chore: try out input mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import { createInputMask, createMaskPattern } from '@solid-primitives/input-mask';
 import { type Component, createSignal } from 'solid-js';
 
+import { BranchIcon } from '@/components/icons/Branch';
 import { GithubIcon } from '@/components/icons/Github';
 import { HeartIcon } from '@/components/icons/Heart';
 import { NamcheeIcon } from '@/components/icons/Namchee';
 import { SolidJsIcon } from '@/components/icons/Solid';
-
 import { ScheduleHint } from '@/components/ScheduleHint';
+
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import { TextField, TextFieldInput } from '@/components/ui/TextField';
-
 import type { ScheduleFormat } from '@/types';
-import { BranchIcon } from './components/icons/Branch';
 
 const Placeholders = {
   cron: '* * * * *',
@@ -39,10 +39,15 @@ const App: Component = () => {
 
         <TextField class="mt-8 w-full">
           <TextFieldInput
-            class="font-mono text-2xl text-center h-16 w-full"
+            class="font-mono text-2xl h-16 w-full"
             value={value()}
-            onInput={e => setValue(e.currentTarget.value)}
-            placeholder={Placeholders[format()]}
+            onInput={e => {
+              createMaskPattern(createInputMask('* * * * *'), () => '* * * * *');
+
+              setValue(e.currentTarget.value);
+            }}
+            spellcheck={false}
+            autocomplete="off"
           />
         </TextField>
 
