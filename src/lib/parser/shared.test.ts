e2bbebb9950afd12995dff6a340adc7665533a41
Cristopher <cristophernamchee12@gmail.com>
2026-03-20 20:25:21 +0700

    test: write test for range parser

--- src/lib/parser/shared.test.ts
+++ src/lib/parser/shared.test.ts
@@ -1,7 +1,6 @@
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
+import { describe, expect, it } from 'vitest';
 
-import { isValidRange } from './shared.js';
+import { isValidRange } from './shared';
 
 describe('isValidRange', () => {
   it('should return false when there are multiple range', () => {
@@ -12,6 +11,61 @@ describe('isValidRange', () => {
     const expected = false;
     const actual = isValidRange(input, min, max);
 
-    assert.equal(expected, actual);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return false when the separated tokens are not numbers', () => {
+    const input = 'foo-2';
+    const min = 0;
+    const max = 59;
+
+    const expected = false;
+    const actual = isValidRange(input, min, max);
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should return false when lower limit is lower than min', () => {
+    const input = '0-5';
+    const min = 1;
+    const max = 59;
+
+    const expected = false;
+    const actual = isValidRange(input, min, max);
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should return false when upper limit is higher than max', () => {
+    const input = '0-4';
+    const min = 0;
+    const max = 3;
+
+    const expected = false;
+    const actual = isValidRange(input, min, max);
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should return false when the upper limit is lower than lower limit', () => {
+    const input = '4-3';
+    const min = 0;
+    const max = 59;
+
+    const expected = false;
+    const actual = isValidRange(input, min, max);
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should return true when the range is valid', () => {
+    const input = '0-5';
+    const min = 0;
+    const max = 59;
+
+    const expected = true;
+    const actual = isValidRange(input, min, max);
+
+    expect(actual).toBe(expected);
   });
 });
--- src/lib/parser/shared.ts
+++ src/lib/parser/shared.ts
@@ -1,5 +1,18 @@
-export function isValidRange(token: string, min: number, max: number): boolean {
-  const tokens = token.split('-');
+/**
+ * Check whether a schedule range is valid or not.
+ *
+ * A schedule is valid if:
+ *  1. It contain 2 numbers, separated by a dash
+ *  2. Both numbers are within the bounds of min and max
+ *  3. Upper bound is greater than equal the lower bound
+ *
+ * @param {string} expr Range expression to evaluate
+ * @param {number} min Minimum number of the lower bound
+ * @param {number} max Maximum number of the upper bound
+ * @returns {boolean} `true` if the range is valid. `false` otherwise.
+ */
+export function isValidRange(expr: string, min: number, max: number): boolean {
+  const tokens = expr.split('-');
 
   if (tokens.length !== 2) {
     return false;
@@ -12,5 +25,5 @@ export function isValidRange(token: string, min: number, max: number): boolean {
     return false;
   }
 
-  return from >= min && from <= max && to >= min && to >= max && from <= to;
+  return from >= min && from <= max && to >= min && to <= max && from <= to;
 }
