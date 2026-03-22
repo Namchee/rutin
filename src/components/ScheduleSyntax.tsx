979b11f1eb37530842e0d3ba52ae56c989811718
Cristopher <cristophernamchee12@gmail.com>
2026-03-22 15:48:57 +0700

    fix: align systemd hint with actual hint

--- src/components/ScheduleSyntax.tsx
+++ src/components/ScheduleSyntax.tsx
@@ -19,8 +19,8 @@ const Macros = {
 };
 
 const MinuteField = { ...CommonOperators, '0 - 59': 'Minute value' };
-const HourField   = { ...CommonOperators, '0 - 23': 'Hour value' };
-const DateField   = { ...CommonOperators, '1 - 31': 'Date value' };
+const HourField = { ...CommonOperators, '0 - 23': 'Hour value' };
+const DateField = { ...CommonOperators, '1 - 31': 'Date value' };
 
 const Syntaxes: Record<ScheduleFormat, Record<string, Record<string, string>>> = {
   unix: {
@@ -34,29 +34,73 @@ const Syntaxes: Record<ScheduleFormat, Record<string, Record<string, string>>> =
       '@reboot': 'After reboot',
     },
     '-1': { ...CommonOperators, ...Macros },
+    '0': MinuteField,
+    '1': HourField,
+    '2': DateField,
+    '3': { ...CommonOperators, '1 - 12': 'Month value', 'JAN - DEC': 'Month value (alt.)' },
+    '4': { ...CommonOperators, '0 - 6': 'Weekday value', 'SUN - SAT': 'Weekday value (alt.)' },
+  },
+  quartz: {
+    '-2': Macros,
+    '-1': { ...CommonOperators, ...Macros },
     '0': { ...CommonOperators, '0 - 59': 'Second value' },
     '1': MinuteField,
     '2': HourField,
     '3': DateField,
     '4': { ...CommonOperators, '1 - 12': 'Month value', 'JAN - DEC': 'Month value (alt.)' },
     '5': { ...CommonOperators, '0 - 6': 'Weekday value', 'SUN - SAT': 'Weekday value (alt.)' },
-  },
-  quartz: {
-    '-2': Macros,
-    '-1': { ...CommonOperators, ...Macros },
-    '0': MinuteField,
-    '1': HourField,
-    '2': DateField,
-    '3': { ...CommonOperators, '1 - 12': 'Month value', 'JAN - DEC': 'Month value (alt.)' },
-    '4': { ...CommonOperators, '0 - 6': 'Weekday value', 'SUN - SAT': 'Weekday value (alt.)' },
+    '6': { ...CommonOperators, '1970 - 2199': 'Year value' },
   },
   systemd: {
-    '-1': { ...CommonOperators, ...Macros },
-    '0': MinuteField,
-    '1': HourField,
-    '2': DateField,
-    '3': { ...CommonOperators, '1 - 12': 'Month value', 'JAN - DEC': 'Month value (alt.)' },
-    '4': { ...CommonOperators, '0 - 6': 'Weekday value', 'SUN - SAT': 'Weekday value (alt.)' },
+    '-2': {
+      minutely: 'Every minute (macro)',
+      hourly: 'Every hour (macro)',
+      daily: 'Every day (macro)',
+      midnight: 'Every day (macro)',
+      weekly: 'Every week (macro)',
+      monthly: 'Every month (macro)',
+      quarterly: 'Every 3 month (macro)',
+      yearly: 'Every year (macro)',
+      annually: 'Every year (macro)',
+    },
+    '-1': {
+      '*': 'Every...',
+      ',': 'Only on...',
+      '..': 'From ... through ...',
+      minutely: 'Every minute (macro)',
+      hourly: 'Every hour (macro)',
+      daily: 'Every day (macro)',
+      midnight: 'Every day (macro)',
+      weekly: 'Every week (macro)',
+      monthly: 'Every month (macro)',
+      quarterly: 'Every 3 month (macro)',
+      yearly: 'Every year (macro)',
+      annually: 'Every year (macro)',
+    },
+    '0': {
+      '*': 'Every...',
+      ',': 'Only on...',
+      '..': 'From ... through ...',
+      '0 - 6': 'Day value',
+      '7': 'Sunday',
+      'Sun - Sat': 'Day value (alt.)',
+    },
+    '1': {
+      '*': 'Every...',
+      ',': 'Only on...',
+      '..': 'From ... through ...',
+      '1970 - 9999': 'Year value',
+      '1 - 12': 'Month value',
+      'JAN - DEC': 'Month value (alt.)',
+      '1 - 31': 'Date value',
+    },
+    '2': {
+      '*': 'Every...',
+      ',': 'Only on...',
+      '..': 'From ... through ...',
+      '0 - 23': 'Hour value',
+      '0 - 59': 'Second / Minute value',
+    },
   },
 };
 
