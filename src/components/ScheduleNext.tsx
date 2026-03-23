9cb3687339bbedb9e77aee1d520b96cd09f41d0c
Cristopher <cristophernamchee12@gmail.com>
2026-03-23 17:00:54 +0700

    feat: add placeholder element

--- src/components/ScheduleNext.tsx
+++ src/components/ScheduleNext.tsx
@@ -1,13 +1,20 @@
-import { Switch, SwitchControl, SwitchLabel, SwitchThumb } from './ui/Switch';
+import { createSignal } from 'solid-js';
+import { Switch, SwitchControl, SwitchLabel, SwitchThumb } from '@/components/ui/Switch';
 
-export function ScheduleNext() {
+interface ScheduleNextProps {
+  expr?: Generator<Date, void, void>;
+}
+
+export function ScheduleNext(props: Readonly<ScheduleNextProps>) {
+  const [next, setNext] = createSignal<Date[]>([]);
+
   return (
     <div class="text-foreground/70 flex flex-col items-center w-full text-sm max-h-md overflow-hidden">
       <p class="h-10 grid place-items-center font-medium text-muted-foreground shrink-0">
         Execution Time
       </p>
 
-      <Switch class="flex gap-2 h-10 items-center mx-auto mt-1">
+      <Switch class="flex gap-2 h-10 items-center mx-auto shrink-0">
         <SwitchLabel class="font-medium">Local Timezone</SwitchLabel>
 
         <SwitchControl>
@@ -17,10 +24,9 @@ export function ScheduleNext() {
         <SwitchLabel class=" font-medium">UTC</SwitchLabel>
       </Switch>
 
-      <div class="text-muted-foreground mt-4 w-full flex flex-col gap-4 overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-        {Array(20)
-          .fill(0)
-          .map(() => (
+      <div class="text-muted-foreground mt-4 w-full flex flex-col gap-4 overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] [mask-image:linear-gradient(to_bottom,black_80%,transparent_100%)] [-webkit-mask-image:linear-gradient(to_bottom,black_80%,transparent_100%)]">
+        {next().length ? (
+          next().map(() => (
             <div class="flex items-center justify-between">
               <p>
                 {new Date('2024-10-24').toLocaleDateString('en-GB', {
@@ -34,13 +40,14 @@ export function ScheduleNext() {
                 })}
               </p>
 
-              <p>
+              <p class="text-balance max-w-12">
                 {new Intl.RelativeTimeFormat('en-GB', { numeric: 'auto' }).format(5, 'minutes')}
               </p>
             </div>
-          ))}
-
-        <div></div>
+          ))
+        ) : (
+          <p>Schedules will appear here when the syntax is valid.</p>
+        )}
       </div>
     </div>
   );
