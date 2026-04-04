d4da673bc05f1c1c141d95b436e6d492e6fac571
Cristopher <cristophernamchee12@gmail.com>
2026-04-04 20:58:20 +0700

    chore: experiment with fading

---
+++ src/components/icons/ChevronRight.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from 'solid-js';
+
+export function ChevronRight(props: JSX.SvgSVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...props}>
+      <path d="m9 18 6-6-6-6" />
+    </svg>
+  );
+}
