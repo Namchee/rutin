a484100b172b1a62c3dab75ef48d4ae2d19a70c4
Cristopher <cristophernamchee12@gmail.com>
2026-04-04 21:27:34 +0700

    refactor: undo masking, you don't need it

--- src/components/FormatSelector.tsx
+++ src/components/FormatSelector.tsx
@@ -1,6 +1,5 @@
 import { For } from 'solid-js';
 import type { ScheduleFormat } from '@/types';
-import { ChevronRight } from './icons/ChevronRight';
 import { TabsList, TabsTrigger } from './ui/Tabs';
 
 const FormatLabel: Record<ScheduleFormat, string> = {
@@ -14,22 +13,16 @@ const FormatLabel: Record<ScheduleFormat, string> = {
 
 export function FormatSelector() {
   return (
-    <div class="max-w-full relative">
-      <div class="relative  overflow-x-auto mx-auto px-8 no-scrollbar [mask-image:linear-gradient(to_right,black_calc(100%-80px),transparent_100%)]">
-        <TabsList class="rounded-full">
-          <For each={Object.entries(FormatLabel)}>
-            {([value, label]) => (
-              <TabsTrigger value={value} class="rounded-full cursor-pointer">
-                {label}
-              </TabsTrigger>
-            )}
-          </For>
-        </TabsList>
-      </div>
-
-      <div class="h-full absolute flex items-center justify-end top-0 right-0 w-20">
-        <ChevronRight class="w-4 h-4 mr-2" />
-      </div>
+    <div class="max-w-full  overflow-x-auto mx-auto px-8 no-scrollbar">
+      <TabsList class="rounded-full">
+        <For each={Object.entries(FormatLabel)}>
+          {([value, label]) => (
+            <TabsTrigger value={value} class="rounded-full cursor-pointer">
+              {label}
+            </TabsTrigger>
+          )}
+        </For>
+      </TabsList>
     </div>
   );
 }
