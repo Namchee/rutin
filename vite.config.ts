aebb4506507af65ab16324ccee39a2e12c0a5151
Cristopher <cristophernamchee12@gmail.com>
2026-04-12 21:41:16 +0700

    chore: setup iconify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import uno from 'unocss/vite';
+import icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [solid(), uno()],
+  plugins: [solid(), uno(), icons({ compiler: 'solid' })],
   server: {
     port: 3000,
   },
