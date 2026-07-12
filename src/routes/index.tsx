f09b5a1d94f4f3b94c2452db3e66f30bfb8bef6d
Cristopher <cristophernamchee12@gmail.com>
2026-07-12 15:27:39 +0700

    feat: basic editor layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,8 +13,38 @@ export default function Home() {
           </p>
         </div>
 
-        <div>
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-[8fr_4fr] lg:gap-8">
+          <div class="flex flex-col gap-4">
+            <div class="flex flex-col rounded-lg border border-separator">
+              <div class="flex items-center justify-between border-separator border-b p-4">
+                <div class="flex items-center gap-2">
+                  <p class="font-medium text-content-tertiary text-sm">Dialect</p>
+                </div>
+              </div>
 
+              <div class="flex-1 p-4"></div>
+            </div>
+
+            <div class="rounded-lg border border-separator">
+              <div class="border-separator border-b p-4">
+                <p class="font-medium text-content-tertiary text-sm">Next executions</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col gap-4">
+            <div class="rounded-lg border border-separator">
+              <div class="border-separator border-b p-4">
+                <p class="font-medium text-content-tertiary text-sm">Field References</p>
+              </div>
+            </div>
+
+            <div class="rounded-lg border border-separator">
+              <div class="border-separator border-b p-4">
+                <p class="font-medium text-content-tertiary text-sm">Macros</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
