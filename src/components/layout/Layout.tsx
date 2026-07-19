fa928c40db4036733e7befd8a54a9cf71cc35e94
Cristopher <cristophernamchee12@gmail.com>
2026-07-19 14:51:35 +0700

    feat: correctly scrollable page on mobile behavior

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,10 +12,10 @@ export function Layout({ children }: Readonly<LayoutProps>) {
     <div class="flex min-h-screen bg-background text-content-primary transition-colors">
       <Sidebar />
 
-      <div class='mx-2 my-2 flex flex-1 flex-col rounded-md border border-separator bg-surface shadow-xs transition-colors md:ml-0'>
+      <div class='mx-2 mb-2 flex flex-1 flex-col rounded-md rounded-b-lg border-separator border-b bg-surface shadow-xs transition-colors md:ml-0'>
         <Topbar />
 
-        <main class="flex-1">
+        <main class="flex-1 border-separator border-x">
           <div class="mx-auto max-w-6xl px-4 py-8">{children}</div>
         </main>
       </div>
--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -5,27 +5,29 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/Tooltip';
 
 export function Topbar() {
   return (
-    <div class='sticky top-0 z-10 flex h-14 items-center justify-between rounded-t-md border-separator border-b bg-surface p-4'>
-      <div>
-        <Tooltip positioning={{ placement: 'right' }}>
-          <TooltipTrigger class="group grid size-6 cursor-pointer place-items-center rounded bg-surface transition-colors hover:bg-surface-hover">
-            <div class="i-lucide-panel-right size-4 bg-content-secondary transition-colors group-hover:bg-content-primary" />
-          </TooltipTrigger>
+    <div class="sticky top-0 z-10 bg-background pt-2">
+      <div class='flex h-14 items-center justify-between rounded-t-lg border border-separator bg-surface p-4'>
+        <div>
+          <Tooltip positioning={{ placement: 'right' }}>
+            <TooltipTrigger class="group grid size-6 cursor-pointer place-items-center rounded bg-surface transition-colors hover:bg-surface-hover">
+              <div class="i-lucide-panel-right size-4 bg-content-secondary transition-colors group-hover:bg-content-primary" />
+            </TooltipTrigger>
 
-          <TooltipContent>Shrink Sidebar</TooltipContent>
-        </Tooltip>
-      </div>
+            <TooltipContent>Shrink Sidebar</TooltipContent>
+          </Tooltip>
+        </div>
 
-      <div class="flex items-center gap-4">
-        <ThemeSwitcher />
+        <div class="flex items-center gap-4">
+          <ThemeSwitcher />
 
-        <A
-          href="https://www.github.com/Namchee/rutin"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="grid size-6 place-items-center rounded text-content-secondary transition-colors hover:bg-background-hover hover:text-content-primary">
-          <div class="i-me-github size-4" />
-        </A>
+          <A
+            href="https://www.github.com/Namchee/rutin"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="grid size-6 place-items-center rounded text-content-secondary transition-colors hover:bg-background-hover hover:text-content-primary">
+            <div class="i-me-github size-4" />
+          </A>
+        </div>
       </div>
     </div>
   );
