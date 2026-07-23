1c400f0b2125c7ae4f7ceabb73855d37966fd907
Cristopher <cristophernamchee12@gmail.com>
2026-07-23 20:34:39 +0700

    fix: do not expand sidebar on initial load on mobile devices

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import { createSignal, type JSX } from 'solid-js';
+
 import { cn } from '@/lib/css';
+import { useMediaQuery } from '@/lib/hooks/use-media-query';
+
 import { Sidebar } from './Sidebar';
 import { Topbar } from './Topbar';
 
@@ -8,7 +11,8 @@ interface LayoutProps {
 }
 
 export function Layout({ children }: Readonly<LayoutProps>) {
-  const [expanded, setExpanded] = createSignal<boolean>(true);
+  const isNonMobile = useMediaQuery('(min-width: 768px)');
+  const [expanded, setExpanded] = createSignal<boolean>(isNonMobile());
 
   return (
     <div class="flex min-h-screen bg-background text-content-primary transition-colors">
@@ -26,7 +30,7 @@ export function Layout({ children }: Readonly<LayoutProps>) {
       <div class="mx-2 mb-2 flex flex-1 flex-col rounded-md rounded-b-lg border-separator border-b bg-surface transition-colors lg:ml-0">
         <Topbar expanded={expanded} setExpanded={setExpanded} />
 
-        <main class='flex-1 rounded-b-lg border-separator border-x transition-colors'>
+        <main class="flex-1 rounded-b-lg border-separator border-x transition-colors">
           <div class="mx-auto max-w-6xl px-4 py-8">{children}</div>
         </main>
       </div>
