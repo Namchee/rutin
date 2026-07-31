b0453df49b93652a1260b27bc6535e5e0eabe102
Cristopher <cristophernamchee12@gmail.com>
2026-07-31 19:16:46 +0700

    chore: adjust 404 and coming soon page to adhere with design system

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -2,23 +2,23 @@ import { A } from '@solidjs/router';
 
 export function ComingSoon() {
   return (
-    <div class="flex h-full flex-col justify-center gap-4">
-      <h1 class="flex items-end gap-1 font-semibold text-5xl tracking-tight md:text-7xl">
+    <div class="flex h-full flex-col justify-center">
+      <h1 class="flex items-end gap-0.5 font-semibold text-2xl leading-snug tracking-tight">
         <span>Baking</span>
-        <span class="inline-flex -translate-y-1 items-baseline gap-1">
-          <span class='![animation-delay:-0.3s] size-2 animate-bounce rounded-full bg-current' />
-          <span class='![animation-delay:-0.15s] size-2 animate-bounce rounded-full bg-current' />
-          <span class="size-2 animate-bounce rounded-full bg-current" />
+        <span class="inline-flex -translate-y-1 items-baseline gap-0.5">
+          <span class="![animation-delay:-0.3s] size-1 animate-bounce rounded-full bg-current" />
+          <span class="![animation-delay:-0.15s] size-1 animate-bounce rounded-full bg-current" />
+          <span class="size-1 animate-bounce rounded-full bg-current" />
         </span>
       </h1>
 
-      <p class="text-2xl text-content-tertiary leading-snug md:text-3xl">
+      <p class='text-content-tertiary text-lg leading-normal'>
         This feature isn't quite out of the oven yet.
       </p>
 
       <A
         href="/"
-        class="group mt-8 inline-flex w-fit items-center gap-1 text-content-secondary transition-colors hover:text-content-primary">
+        class="group mt-8 inline-flex w-fit items-center gap-1 text-content-secondary text-sm transition-colors hover:text-content-primary">
         <span>Bring me home</span>{' '}
         <div class="i-lucide-arrow-right size-4 transition-all group-hover:translate-x-1" />
       </A>
--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,16 +2,14 @@ import { A } from '@solidjs/router';
 
 export default function NotFound() {
   return (
-    <div class="flex h-full flex-col justify-center gap-4">
-      <h1 class='font-semibold text-5xl tracking-tight md:text-7xl'>404</h1>
+    <div class="flex h-full flex-col justify-center">
+      <h1 class="font-semibold text-2xl leading-snug tracking-tight">404</h1>
 
-      <p class="text-2xl text-content-tertiary leading-none md:text-3xl">
-        Yep... you are totally lost.
-      </p>
+      <p class="text text-content-tertiary text-lg">Yep... you are totally lost.</p>
 
       <A
         href="/"
-        class="group mt-8 inline-flex w-fit items-center gap-1 text-content-secondary transition-colors hover:text-content-primary">
+        class='group mt-8 inline-flex w-fit items-center gap-1 text-content-secondary text-sm transition-colors hover:text-content-primary'>
         <span>Bring me home</span>{' '}
         <div class="i-lucide-arrow-right size-4 transition-all group-hover:translate-x-1" />
       </A>
