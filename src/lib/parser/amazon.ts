2ed338d47667ce03103328d2504fd1d05ff1e880
Cristopher <cristophernamchee12@gmail.com>
2026-08-05 21:23:10 +0700

    feat: add amazon parser

---
+++ src/lib/parser/amazon.ts
@@ -0,0 +1,63 @@
+import { createScheduleParser } from './base';
+import { createTokenValidator } from './validator';
+
+const DayToNumber = {
+  fri: 5,
+  mon: 1,
+  sat: 6,
+  sun: 0,
+  thu: 4,
+  tue: 2,
+  wed: 3,
+};
+
+const MonthToNumber = {
+  apr: 4,
+  aug: 8,
+  dec: 12,
+  feb: 2,
+  jan: 1,
+  jul: 7,
+  jun: 6,
+  mar: 3,
+  may: 5,
+  nov: 11,
+  oct: 10,
+  sep: 9,
+};
+
+export const AmazonParser = createScheduleParser({
+  fieldOrder: ['minute', 'hour', 'dayOfMonth', 'month', 'dayOfWeek', 'year'],
+  fields: {
+    dayOfMonth: { max: 31, min: 1 },
+    // 7 is Sunday
+    dayOfWeek: { aliases: DayToNumber, max: 7, min: 0 },
+    hour: { max: 23, min: 0 },
+    minute: { max: 59, min: 0 },
+    month: { aliases: MonthToNumber, max: 12, min: 1 },
+    year: { max: 2199, min: 1970 },
+  },
+  tokenizer: (expr: string) => {
+    const tokens = Array.from(expr.trim().matchAll(/\S+/g), match => ({
+      position: [match.index, match.index + match[0].length] as [number, number],
+      value: match[0],
+    }));
+
+    return {
+      dayOfMonth: tokens[2],
+      dayOfWeek: tokens[4],
+      hour: tokens[1],
+      minute: tokens[0],
+      month: tokens[3],
+      year: tokens[5],
+    };
+  },
+  validators: {
+    dayOfMonth: createTokenValidator(/[^0-9*,\-/?]/i, 1, 31),
+    dayOfWeek: createTokenValidator(/[^0-9*,\-/L#?]/i, 0, 7),
+    hour: createTokenValidator(/[^0-9*,\-/]/, 0, 23),
+    minute: createTokenValidator(/[^0-9*,\-/]/, 0, 59),
+    month: createTokenValidator(/[^0-9*,\-/]/, 1, 12),
+    year: createTokenValidator(/[^0-9*,\-/]/, 1970, 2199),
+  },
+});
--- src/lib/parsers.ts
+++ src/lib/parsers.ts
@@ -1,11 +1,12 @@
 import type { ScheduleFormat } from '@/types';
-
+import { AmazonParser } from './parser/amazon';
 import { CloudflareWorkersParser } from './parser/cf-workers';
 import { NodeParser } from './parser/node';
 import type { ScheduleParser } from './parser/types';
 import { UNIXParser } from './parser/unix';
 
 export const Parsers: Record<ScheduleFormat, ScheduleParser> = {
+  amazon: AmazonParser,
   'cf-workers': CloudflareWorkersParser,
   node: NodeParser,
   unix: UNIXParser,
